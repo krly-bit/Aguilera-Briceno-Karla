45a12dda2e95c4521b92b4ce81cc86dfaca8b909
karlita29 <lyversis@gmail.com>
2021-06-14 12:46:58 -0300

    creacion itemList, problemas cambio de estado

---
+++ espacio-color/src/components/ItemList/ItemList.js
@@ -0,0 +1,48 @@
+import {useState, useEffect} from "react";
+ function ItemList () {
+
+   let products =[
+        {id:"1", name:"agendas-minimal",  stock:"10", category:"libreria", description:"agenda con diseños minimalistas", models:["violeta", "amarillo", "azul", "verde agua", "verde musgo", "naranja"] },
+        {id:"2", name: "móvil metálico",  stock:"20", category:"deco", description:"Móvio metalico con sonidos", models:"móvil metálico flautas"},
+        {id:"3", name:"movil de semillas",stock:"20", category:"deco", description:"Móvil de semillas", models:["colores tropicales", "colores marinos", "colores puesta de sol", " colores noche"]},]
+
+const [catalogo, setCatalogo] = useState([{ id:"", name:" ", stock:"", category:"", description:"", models:""}]);
+
+useEffect( ()=>{
+ new Promise( (resolve, reject)=>{
+      setTimeout(resolve({result:products}, 3000))
+  } )
+  
+  .then( 
+      data=>{ if (data.result){
+        console.log(data.result)
+        setCatalogo(data.result)
+        console.log(catalogo)
+      } else{
+          throw new Error()
+      }
+
+      },
+      error=>{
+
+        console.log("Ha ocurrido un error")
+
+      }
+  )
+  .catch(error=>{ console.log(error.message);
+    return "No hay productos disponibles"
+} ) 
+. then ( msg=>{ return msg;})}, [] ) 
+
+return  <div> {catalogo.map(element=>{ 
+<div className="itemList" key={element.id}>
+<ul>
+            <li> {element.name}</li>
+            <li> {element.category}</li>
+            <li>  {element.stock}</li>
+        </ul>
+         </div>} ) } </div> 
+         
+        }
+        
+        export default ItemList
--- espacio-color/src/components/ItemListContainer/ItemListContainer.js
+++ espacio-color/src/components/ItemListContainer/ItemListContainer.js
@@ -2,12 +2,14 @@
 
 import * as ReactBootstrap from "react-bootstrap";
 import Counter from '../counter/Counter';
+import ItemList from '../ItemList/ItemList';
 
 function ItemListContainer (props)
 { 
         return  <ReactBootstrap.Container fluid> 
 <h2 >{props.greeting} </h2>
 <Counter stock={5} startCounter={1}></Counter>
+<ItemList></ItemList>
  </ReactBootstrap.Container> 
  
 
