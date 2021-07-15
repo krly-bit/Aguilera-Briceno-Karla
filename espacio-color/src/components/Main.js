2f8d059994e8a855f1c517b550611983ec1fa629
fresia briceño lazcano <lyversis@gmail.com>
2021-07-14 23:15:19 -0300

    Entrega firebase 2

--- espacio-color/src/components/Main.js
+++ espacio-color/src/components/Main.js
@@ -9,11 +9,43 @@ import React, {useContext} from 'react';
 import {useState} from 'react';
 import CartContext from '../context/CartContext'
 import CartProvider from '../provider/CartProvider'
+import firebase from 'firebase/app'
+import { getFirestore } from '../firebase/firebase.js'
+
 
 
 function Main () {
 
+ const db=getFirestore();
+ const order=db.collection('orders');
+
+ const newOrder={
+
+    buyer:{
+        mail:'kar.aguilera.bri@gmail.com',
+        name:'Karly',
+        phone:'12345'},
+    date: firebase.firestore.Timestamp.fromDate( new Date()),
+
+   items:[{
+       id:'ssgghh',
+       price: 5000,
+       title:' agenda full color'
+   }],
+   total: 5000
+
+
+ }
+
+ order.add(newOrder)
+ .then(({id}) =>
+     console.log(`Elemento creado con Id ${id}`))
  
+.catch( error=> console.log(error))
+
+
+ 
+
 
     return (
         
